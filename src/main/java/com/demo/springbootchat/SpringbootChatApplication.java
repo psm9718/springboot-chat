5f1b7dacf05b09986b6bbfc34e56fb2ee0edba96
psm9718 <psm9718@gmail.com>
2023-03-07 09:25:42 +0900

    fix: 프로젝트 목표 수정에 따라 네이밍 변경

--- src/main/java/com/demo/springbootchat/SpringbootChatApplication.java
+++ src/main/java/com/demo/springbootchat/SpringbootChatApplication.java
@@ -1,13 +1,13 @@
-package com.demo.springbootpractice;
+package com.demo.springbootchat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootPracticeApplication {
+public class SpringbootChatApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootPracticeApplication.class, args);
+        SpringApplication.run(SpringbootChatApplication.class, args);
     }
 
 }
